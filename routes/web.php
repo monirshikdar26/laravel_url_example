9de8501ab341469d1a4cff2ba4b0bff0599fd382
monirshikdar26 <monirshikdar26@gamil.com>
2021-07-27 19:52:33 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\GalaryController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/features', function () {
+    return view('features');
+});
+
+Route::get('/all-company-picture',[GalaryController::class,'show'])->name('pic');
